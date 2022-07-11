1312865575e088618b7ea9c79d19cf96bc34507d
19178910 <KNMelnik@sberbank.com>
2022-07-11 12:58:08 +0700

    Command - исправил тест

--- src/test/java/pattern/behavioral/command/TextFileOperationTest.java
+++ src/test/java/pattern/behavioral/command/TextFileOperationTest.java
@@ -11,23 +11,21 @@ public class TextFileOperationTest {
         TextFileOperationExecutor textFileOperationExecutor = new TextFileOperationExecutor();
         String resultOne = textFileOperationExecutor.executeOperation(new OpenTextFileOperation(new TextFile("file1.txt")));
 
-        assertEquals(resultOne, "Opening file file1.txt");
-
+        assertEquals("Opening file file1.txt", resultOne);
     }
 
     @Test
     public void executeTwoTest() {
         TextFileOperationExecutor textFileOperationExecutor = new TextFileOperationExecutor();
         String resultTwo = textFileOperationExecutor.executeOperation(new SaveTextFileOperation(new TextFile("file2.txt")));
-        assertEquals(resultTwo, "Opening file file1.txt");
-
+        assertEquals("Saving file file2.txt", resultTwo);
     }
 
     @Test
     public void executeLambdaTest() {
         TextFileOperationExecutor textFileOperationExecutor = new TextFileOperationExecutor();
         String resultTwo = textFileOperationExecutor.executeOperation(() -> "Result Lambda");
-        assertEquals(resultTwo, "Result Lambda");
+        assertEquals("Result Lambda", resultTwo);
     }
 
 }
